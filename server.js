e692ce45f87214fd03a8dcb6c9b91a616341b80d
presleedees-glitch <preslee.dees@attala.k12.ms.us>
2026-03-20 08:14:36 -0500

    sadasdasfwewf

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-// server.js
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
-const Kahoot = require('kahoot.js-updated');
+// Kahoot client temporarily disabled to allow server to start
+// const Kahoot = require('kahoot.js-updated');
 
 const app = express();
 app.use(express.static('.'));
@@ -14,37 +14,17 @@ wss.on('connection', ws => {
   ws.on('message', async raw => {
     try {
       const msg = JSON.parse(raw);
+
       if (msg.action === 'join') {
-        const client = new Kahoot();
-        ws.kahootClient = client;
-
-        client.on('joined', () => ws.send(JSON.stringify({ event: 'joined' })));
-        client.on('quizStart', quiz => ws.send(JSON.stringify({ event: 'quizStart', quiz })));
-        client.on('questionStart', q => ws.send(JSON.stringify({ event: 'questionStart', question: q })));
-        client.on('questionEnd', () => ws.send(JSON.stringify({ event: 'questionEnd' })));
-        client.on('quizEnd', () => ws.send(JSON.stringify({ event: 'quizEnd' })));
-        client.on('error', err => ws.send(JSON.stringify({ event: 'error', message: String(err) })));
-
-        try {
-          await client.join(msg.pin, msg.nick);
-        } catch (err) {
-          ws.send(JSON.stringify({ event: 'error', message: String(err) }));
-        }
+        // Kahoot client disabled for now
+        ws.send(JSON.stringify({ event: 'info', message: 'Kahoot client disabled for testing' }));
       }
 
-      if (msg.action === 'answer' && ws.kahootClient && ws.kahootClient.question) {
-        // msg.choice is 0-based index of answer
-        try {
-          ws.kahootClient.question.answer(msg.choice);
-          ws.send(JSON.stringify({ event: 'answered', choice: msg.choice }));
-        } catch (err) {
-          ws.send(JSON.stringify({ event: 'error', message: String(err) }));
-        }
+      if (msg.action === 'answer') {
+        ws.send(JSON.stringify({ event: 'answered', choice: msg.choice }));
       }
 
-      if (msg.action === 'leave' && ws.kahootClient) {
-        try { ws.kahootClient.leave(); } catch(e){}
-        delete ws.kahootClient;
+      if (msg.action === 'leave') {
         ws.send(JSON.stringify({ event: 'left' }));
       }
     } catch (e) {
@@ -52,9 +32,7 @@ wss.on('connection', ws => {
     }
   });
 
-  ws.on('close', () => {
-    if (ws.kahootClient) try { ws.kahootClient.leave(); } catch(e){}
-  });
+  ws.on('close', () => {});
 });
 
 server.listen(3000, () => console.log('Server listening on http://localhost:3000'));
